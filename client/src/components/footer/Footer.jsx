96c2c177ec39bc04b400a7a96b20bbbfd3cdca73
tanmaiii <tanmai833@gmail.com>
2023-10-20 12:07:57 +0700

    footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
-import './footer.scss'
+import React from "react";
+import "./footer.scss";
+import img from "../../assets/images/logoJobQuest.png";
+import { BiSolidSend } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className='footer'>Footer</div>
-  )
+    <div className="footer">
+      <div className="container">
+        <div className="footer__list row">
+          <div className="footer__list__col col pc-6">
+            <div className="footer__list__col__logo">
+              <img src={img} alt="" />
+              <h2>JobQuest</h2>
+            </div>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quis
+              iste, sunt eos debitis est impedit ipsam facilis qui rem
+              voluptatibus vel officiis saepe, fugiat tempora inventore omnis,
+              unde id.
+            </span>
+            <div className="footer__list__col__input">
+              <i class="fa-regular fa-envelope"></i>
+              <input type="text" placeholder="Địa chỉ Email..." />
+              <button>
+                <BiSolidSend />
+              </button>
+            </div>
+          </div>
+          <div className="footer__list__col col pc-2">
+            <h4 className="footer__list__col__header">About Us</h4>
+            <ul className="footer__list__col__list">
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Công ty</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__list__col col pc-2">
+            <h4 className="footer__list__col__header">About Us</h4>
+            <ul className="footer__list__col__list">
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Công ty</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__list__col col pc-2">
+            <h4 className="footer__list__col__header">About Us</h4>
+            <ul className="footer__list__col__list">
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Công ty</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+              <li>
+                <Link>Việc làm</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer__bottom"></div>
+      </div>
+    </div>
+  );
 }
--- client/src/components/sectionList/SectionList.jsx
+++ client/src/components/sectionList/SectionList.jsx
@@ -5,74 +5,16 @@ import ItemCompany from "../itemCompany/ItemCompany";
 import companies from "../../config/companies";
 
 export default function SectionList({ title }) {
-  const [btnLeft, setBtnLeft] = useState(false);
-  const [btnRight, setBtnRight] = useState(true);
 
   const sectionListRef = useRef();
-  const btnRightTimeoutRef = useRef();
-  const btnLeftTimeoutRef = useRef();
-
-  const handleTimeoutLeft = () => {
-    if (btnLeftTimeoutRef.current) {
-      clearTimeout(btnLeftTimeoutRef.current);
-    }
-    btnLeftTimeoutRef.current = setTimeout(() => {
-      handleClickLeft();
-    }, 300);
-  };
-
-  const handleTimeoutRight = () => {
-    if (btnRightTimeoutRef.current) {
-      clearTimeout(btnRightTimeoutRef.current);
-    }
-    btnRightTimeoutRef.current = setTimeout(() => {
-      handleClickRight();
-    }, 300);
-  };
-
-  const handleClickRight = () => {
-    const widthSection = sectionListRef.current.clientWidth;
-    sectionListRef.current.scrollLeft += widthSection;
-    handleHideBtn();
-  };
-
-  const handleClickLeft = () => {
-    const widthSection = sectionListRef.current.clientWidth;
-    sectionListRef.current.scrollLeft -= widthSection;
-    handleHideBtn();
-  };
-
-  const handleHideBtn = () => {
-    setBtnLeft(sectionListRef.current.scrollLeft > 0 ? true : false);
-    setBtnRight(
-      sectionListRef.current.scrollLeft <
-        sectionListRef.current.scrollWidth - sectionListRef.current.clientWidth
-        ? true
-        : false
-    );
-  };
 
   return (
     <div className="sectionList">
       <div className="sectionList__header">
         <h4>{title}</h4>
-        <Link className="sectionCategories__header__link">
-          <span>Xem tất cả</span> <i className="fa-solid fa-chevron-right"></i>
-        </Link>
       </div>
       <div className="sectionList__body">
-        {btnLeft && (
-          <button
-            className=" sectionList__body__btn sectionList__body__left"
-            onClick={() => handleTimeoutLeft()}
-          >
-            <i className="fa-solid fa-chevron-left"></i>
-          </button>
-        )}
-        <div
-          ref={sectionListRef}
-          className=" sectionList__body__list row"
-        >
+        <div ref={sectionListRef} className=" sectionList__body__list row">
           {companies &&
             companies.map((company, i) => (
               <ItemCompany
@@ -82,14 +24,11 @@ export default function SectionList({ title }) {
               />
             ))}
         </div>
-        {btnRight && (
-          <button
-            className=" sectionList__body__btn sectionList__body__right"
-            onClick={() => handleTimeoutRight()}
-          >
-            <i className="fa-solid fa-chevron-right"></i>
-          </button>
-        )}
+      </div>
+      <div className="sectionList__btn">
+        <button className="">
+          <span>Xem tất cả</span> <i className="fa-solid fa-chevron-right"></i>
+        </button>
       </div>
     </div>
   );
