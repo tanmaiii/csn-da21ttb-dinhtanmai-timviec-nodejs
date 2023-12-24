c8242104569eaec54d425af08a506b44354b0411
tanmaiii <tanmai833@gmail.com>
2023-12-24 22:23:45 +0700

    env

--- src/client/src/axios.js
+++ src/client/src/axios.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
+//https://csn-api.onrender.com
 //http://localhost:8800
 //https://nodejs-mysql-y4nx.onrender.com
 //https://api-job.vercel.app
-const baseURL = "http://localhost:8800";
+
+console.log(process.env);
+
+const baseURL = process.env.REACT_APP_API_URL;
+
 
 export const apiImage = `${baseURL}/images/`;
 
 export const apiCv = `${baseURL}/cv/`;
 
 export const makeRequest = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   withCredentials: true,
 });
