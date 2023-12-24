0e85048afd877c772c7dc394fd7b5af8d0e42cb1
tanmaiii <tanmai833@gmail.com>
2023-12-24 22:29:33 +0700

    env

--- src/client/src/axios.js
+++ src/client/src/axios.js
@@ -1,14 +1,6 @@
 import axios from "axios";
 
-//https://csn-api.onrender.com
-//http://localhost:8800
-//https://nodejs-mysql-y4nx.onrender.com
-//https://api-job.vercel.app
-
-console.log(process.env);
-
 const baseURL = process.env.REACT_APP_API_URL;
-
 
 export const apiImage = `${baseURL}/images/`;
 
