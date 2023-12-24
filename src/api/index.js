1896cc6022acc1112f25976323bf7cb86c712ee9
tanmaiii <tanmai833@gmail.com>
2023-12-24 23:24:19 +0700

    change cors

--- src/api/index.js
+++ src/api/index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "jobquest-tm.vercel.app"],
+    origin: ["http://localhost:3000", "https://jobquest-tm.vercel.app"],
   })
 );
 
