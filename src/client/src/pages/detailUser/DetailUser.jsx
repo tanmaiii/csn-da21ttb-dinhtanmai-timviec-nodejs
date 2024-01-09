14041bd32c24f476737954b128d6cb7f319aa972
tanmaiii <tanmai833@gmail.com>
2024-01-09 16:12:41 +0700

    update detail user, company

--- src/client/src/pages/detailUser/DetailUser.jsx
+++ src/client/src/pages/detailUser/DetailUser.jsx
@@ -38,7 +38,7 @@ export default function DetailUser() {
   const controlMbRef = useRef();
   const modalAvatarRef = useRef();
   const { currentUser } = useAuth();
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(false);
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { pathname } = useLocation();
@@ -47,12 +47,15 @@ export default function DetailUser() {
   const controlPathname = pathname.split("/").filter(Boolean).pop();
 
   const getUser = async () => {
+    setLoading(true);
     try {
       const res = await makeRequest.get("/user/find/" + id);
       setUser(res.data);
+      setLoading(false);
     } catch (error) {
       setErr("id không đúng");
     }
+    setLoading(false);
   };
 
   const { isLoading, error, data } = useQuery(["user", id], () => {
@@ -90,199 +93,200 @@ export default function DetailUser() {
 
   return (
     <div>
-      {err && <NotFound />}
-      {!user && <Loader/>}
       <div className="detailUser">
         <div className="container">
           <div className="detailUser__wrapper">
-            {/* {loading ? (
+            {loading ? (
               <Loader />
-            ) : ( */}
-             { user && (
-                <>
-                  <div className="detailUser__wrapper__header">
-                    <div className="detailUser__wrapper__header__bg">
-                      <img className="image-loading" src={bg} alt="" />
-                    </div>
-                    <div className="detailUser__wrapper__header__main">
-                      <div className="detailUser__wrapper__header__main__left">
-                        <div className="detailUser__wrapper__header__main__left__image">
-                          <img
-                            className="image-loading"
-                            onClick={() => setOpenModalAvatar(true)}
-                            src={user?.avatarPic ? apiImage + user?.avatarPic : avatar}
-                            alt=""
-                          />
-                          {user?.id === currentUser?.id && (
-                            <button
-                              className="detailUser__wrapper__header__main__left__image__edit"
-                              onClick={() => setOpenModalEditAvatar(true)}
-                            >
-                              <i className="fa-solid fa-camera"></i>
-                            </button>
-                          )}
-                        </div>
-                        <div className="detailUser__wrapper__header__main__left__text">
-                          <span className="tag">Người dùng</span>
-                          <h4 className="name">{user?.name}</h4>
-                          <div className="date">
-                            <i className="fa-solid fa-calendar-days"></i>
-                            <span>
-                              {user?.birthDay
-                                ? moment(user?.birthDay).format("DD/MM/YYYY")
-                                : "00/00/0000"}
-                            </span>
-                          </div>
+            ) : user ? (
+              <>
+                <div className="detailUser__wrapper__header">
+                  <div className="detailUser__wrapper__header__bg">
+                    <img className="image-loading" src={bg} alt="" />
+                  </div>
+                  <div className="detailUser__wrapper__header__main">
+                    <div className="detailUser__wrapper__header__main__left">
+                      <div className="detailUser__wrapper__header__main__left__image">
+                        <img
+                          className="image-loading"
+                          onClick={() => setOpenModalAvatar(true)}
+                          src={user?.avatarPic ? apiImage + user?.avatarPic : avatar}
+                          alt=""
+                        />
+                        {user?.id === currentUser?.id && (
+                          <button
+                            className="detailUser__wrapper__header__main__left__image__edit"
+                            onClick={() => setOpenModalEditAvatar(true)}
+                          >
+                            <i className="fa-solid fa-camera"></i>
+                          </button>
+                        )}
+                      </div>
+                      <div className="detailUser__wrapper__header__main__left__text">
+                        <span className="tag">Người dùng</span>
+                        <h4 className="name">{user?.name}</h4>
+                        <div className="date">
+                          <i className="fa-solid fa-calendar-days"></i>
+                          <span>
+                            {user?.birthDay
+                              ? moment(user?.birthDay).format("DD/MM/YYYY")
+                              : "00/00/0000"}
+                          </span>
                         </div>
                       </div>
-                      {user?.id === currentUser?.id && (
-                        <Link to={`/nguoi-dung/${currentUser?.id}/info`}>
-                          <div className="detailUser__wrapper__header__main__button">
-                            <button>Chỉnh sửa</button>
-                          </div>
-                        </Link>
-                      )}
                     </div>
-                  </div>
-                  <div className="detailUser__wrapper__body row">
-                    <div className=" col pc-9 t-8 m-12">
-                      <div className="detailUser__wrapper__body__left">
-                        <div className="detailUser__wrapper__body__left__control">
-                          <Link
-                            to={`/nguoi-dung/${id}/`}
-                            className={`${controlPathname === id && "active"}`}
-                          >
-                            <span>Giới thiệu</span>
-                          </Link>
-
-                          {user?.id === currentUser?.id && (
-                            <>
-                              <Link
-                                to={`/nguoi-dung/${id}/info`}
-                                className={`${controlPathname === "info" && "active"}`}
-                              >
-                                <span>Thông tin</span>
-                              </Link>
-                              <Link
-                                to={`/nguoi-dung/${id}/apply`}
-                                className={`${controlPathname === "apply" && "active"}`}
-                              >
-                                <span>Ứng tuyển</span>
-                              </Link>
-                              <Link
-                                to={`/nguoi-dung/${id}/jobs`}
-                                className={`${controlPathname === "jobs" && "active"}`}
-                              >
-                                <span>Việc làm</span>
-                              </Link>
-                              <Link
-                                to={`/nguoi-dung/${id}/companies`}
-                                className={`${controlPathname === "companies" && "active"}`}
-                              >
-                                <span>Theo dõi</span>
-                              </Link>
-                            </>
-                          )}
+                    {user?.id === currentUser?.id && (
+                      <Link to={`/nguoi-dung/${currentUser?.id}/info`}>
+                        <div className="detailUser__wrapper__header__main__button">
+                          <button>Chỉnh sửa</button>
                         </div>
+                      </Link>
+                    )}
+                  </div>
+                </div>
+                <div className="detailUser__wrapper__body row">
+                  <div className=" col pc-9 t-8 m-12">
+                    <div className="detailUser__wrapper__body__left">
+                      <div className="detailUser__wrapper__body__left__control">
+                        <Link
+                          to={`/nguoi-dung/${id}/`}
+                          className={`${controlPathname === id && "active"}`}
+                        >
+                          <span>Giới thiệu</span>
+                        </Link>
 
-                        <div className="detailUser__wrapper__body__left__control-mobile">
-                          <Link
-                            to={`/nguoi-dung/${id}/`}
-                            className={`${controlPathname === id && "active"}`}
-                          >
-                            <span>Giới thiệu</span>
-                          </Link>
-                          {currentUser?.id === user.id && (
+                        {user?.id === currentUser?.id && (
+                          <>
                             <Link
                               to={`/nguoi-dung/${id}/info`}
                               className={`${controlPathname === "info" && "active"}`}
                             >
                               <span>Thông tin</span>
                             </Link>
-                          )}
-                          {currentUser?.id === user.id && (
-                            <div className="button__more" ref={controlMbRef}>
-                              <button
-                                className={`button__more__toggle ${openControlMb && "active"}`}
-                                onClick={() => setOpenControlMb(!openControlMb)}
-                              >
-                                <span>Thêm</span>
-                                <i className="fa-solid fa-angle-down"></i>
-                              </button>
-                              {openControlMb && (
-                                <div className="button__more__dropdown">
-                                  <Link
-                                    to={`/nguoi-dung/${id}/apply`}
-                                    className={`${controlPathname === "apply" && "active"}`}
-                                  >
-                                    <span>Ứng tuyển</span>
-                                  </Link>
-                                  <Link
-                                    to={`/nguoi-dung/${id}/jobs`}
-                                    className={`${controlPathname === "jobs" && "active"}`}
-                                  >
-                                    <span>Công việc</span>
-                                  </Link>
-                                  <Link
-                                    to={`/nguoi-dung/${id}/companies`}
-                                    className={`${controlPathname === "companies" && "active"}`}
-                                  >
-                                    <span>Công ty</span>
-                                  </Link>
-                                </div>
-                              )}
-                            </div>
-                          )}
-                        </div>
+                            <Link
+                              to={`/nguoi-dung/${id}/apply`}
+                              className={`${controlPathname === "apply" && "active"}`}
+                            >
+                              <span>Ứng tuyển</span>
+                            </Link>
+                            <Link
+                              to={`/nguoi-dung/${id}/jobs`}
+                              className={`${controlPathname === "jobs" && "active"}`}
+                            >
+                              <span>Việc làm</span>
+                            </Link>
+                            <Link
+                              to={`/nguoi-dung/${id}/companies`}
+                              className={`${controlPathname === "companies" && "active"}`}
+                            >
+                              <span>Theo dõi</span>
+                            </Link>
+                          </>
+                        )}
+                      </div>
 
-                        <div className="detailUser__wrapper__body__left__content">
-                          <Routes>
-                            <Route index element={<IntroUser intro={user?.intro} />} />
-                            <Route path="info" element={<InfoUser />} />
-                            <Route path="apply" element={<AppliedJobs />} />
-                            <Route path="jobs" element={<JobsSave />} />
-                            <Route path="companies" element={<CompaniesSave />} />
-                          </Routes>
-                        </div>
+                      <div className="detailUser__wrapper__body__left__control-mobile">
+                        <Link
+                          to={`/nguoi-dung/${id}/`}
+                          className={`${controlPathname === id && "active"}`}
+                        >
+                          <span>Giới thiệu</span>
+                        </Link>
+                        {currentUser?.id === user.id && (
+                          <Link
+                            to={`/nguoi-dung/${id}/info`}
+                            className={`${controlPathname === "info" && "active"}`}
+                          >
+                            <span>Thông tin</span>
+                          </Link>
+                        )}
+                        {currentUser?.id === user.id && (
+                          <div className="button__more" ref={controlMbRef}>
+                            <button
+                              className={`button__more__toggle ${openControlMb && "active"}`}
+                              onClick={() => setOpenControlMb(!openControlMb)}
+                            >
+                              <span>Thêm</span>
+                              <i className="fa-solid fa-angle-down"></i>
+                            </button>
+                            {openControlMb && (
+                              <div className="button__more__dropdown">
+                                <Link
+                                  to={`/nguoi-dung/${id}/apply`}
+                                  className={`${controlPathname === "apply" && "active"}`}
+                                >
+                                  <span>Ứng tuyển</span>
+                                </Link>
+                                <Link
+                                  to={`/nguoi-dung/${id}/jobs`}
+                                  className={`${controlPathname === "jobs" && "active"}`}
+                                >
+                                  <span>Công việc</span>
+                                </Link>
+                                <Link
+                                  to={`/nguoi-dung/${id}/companies`}
+                                  className={`${controlPathname === "companies" && "active"}`}
+                                >
+                                  <span>Công ty</span>
+                                </Link>
+                              </div>
+                            )}
+                          </div>
+                        )}
                       </div>
-                    </div>
-                    <div className="col pc-3 t-4 m-12">
-                      <div className="detailUser__wrapper__body__right">
-                        <h4>Thông tin</h4>
-                        <div className="facebook">
-                          <i className="fa-brands fa-facebook"></i>
-                          {user?.linkSocial?.split("/")[3] ? (
-                            <a href={user?.linkSocial}>{user?.linkSocial}</a>
-                          ) : (
-                            <span>Không có</span>
-                          )}
-                        </div>
-                        <div className="mail">
-                          <i className="fa-regular fa-envelope"></i>
-                          <a href={`mailto:${user?.email}`}>{user?.email}</a>
-                        </div>
-                        <div className="province">
-                          <i className="fa-solid fa-location-dot"></i>
-                          {user?.province ? (
-                            <span href="">{user?.province}</span>
-                          ) : (
-                            <span>Không có</span>
-                          )}
-                        </div>
+
+                      <div className="detailUser__wrapper__body__left__content">
+                        <Routes>
+                          <Route index element={<IntroUser intro={user?.intro} />} />
+                          <Route path="info" element={<InfoUser />} />
+                          <Route path="apply" element={<AppliedJobs />} />
+                          <Route path="jobs" element={<JobsSave />} />
+                          <Route path="companies" element={<CompaniesSave />} />
+                        </Routes>
                       </div>
-                      <RecomKeywork />
                     </div>
                   </div>
-                </>
-              )}
-            {/* )}  */}
+                  <div className="col pc-3 t-4 m-12">
+                    <div className="detailUser__wrapper__body__right">
+                      <h4>Thông tin</h4>
+                      <div className="facebook">
+                        <i className="fa-brands fa-facebook"></i>
+                        {user?.linkSocial?.split("/")[3] ? (
+                          <a href={user?.linkSocial}>{user?.linkSocial}</a>
+                        ) : (
+                          <span>Không có</span>
+                        )}
+                      </div>
+                      <div className="mail">
+                        <i className="fa-regular fa-envelope"></i>
+                        <a href={`mailto:${user?.email}`}>{user?.email}</a>
+                      </div>
+                      <div className="province">
+                        <i className="fa-solid fa-location-dot"></i>
+                        {user?.province ? (
+                          <span href="">{user?.province}</span>
+                        ) : (
+                          <span>Không có</span>
+                        )}
+                      </div>
+                    </div>
+                    <RecomKeywork />
+                  </div>
+                </div>
+              </>
+            ) : (
+              <NotFound />
+            )}
           </div>
         </div>
       </div>
       <ModalCropImage openModal={openModalEditAvatar} setOpenModal={setOpenModalEditAvatar} />
 
-      <ModalAvatar openModal={openModalAvatar} setOpenModal={setOpenModalAvatar} avatarPic={user?.avatarPic ? apiImage + user?.avatarPic : avatar} />
-      
+      <ModalAvatar
+        openModal={openModalAvatar}
+        setOpenModal={setOpenModalAvatar}
+        avatarPic={user?.avatarPic ? apiImage + user?.avatarPic : avatar}
+      />
     </div>
   );
 }
